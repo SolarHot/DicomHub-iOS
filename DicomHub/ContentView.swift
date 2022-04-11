4fa07743114aba1895d85bf3d31406a885c2bb46
lp <lipeng@zhenhealth.cn>
2022-04-11 22:48:16 +0800

    new project

---
+++ DicomHub/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DicomHub
+//
+//  Created by 李鹏 on 2022/4/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DicomHub/DicomHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  DicomHubApp.swift
+//  DicomHub
+//
+//  Created by 李鹏 on 2022/4/11.
+//
+
+import SwiftUI
+
+@main
+struct DicomHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
